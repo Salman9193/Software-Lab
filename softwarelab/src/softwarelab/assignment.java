0407424874f83e65bf358284acbd3a07701fad7d
salman <salman@192.168.18.51>
2014-09-04 12:47:39 -0700

    COMMENTED

--- softwarelab/src/softwarelab/assignment.java
+++ softwarelab/src/softwarelab/assignment.java
@@ -4,10 +4,28 @@ public class assignment {
 
 	int multiply(int a,int b)
 	{
-		return a*b;
+		if(a==1)
+		{
+			return b;
+		}
+		else if(b==1)
+		{
+			return a;
+		}
+		else
+		{
+			return a*b;
+		}
+		
 	}
 	int add(int a,int b)
 	{
-		
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		assignment a = new assignment();
+		//System.out.println(a.add(3, 7));
+		//System.out.println(a.multiply(1, 1));
 	}
 }
